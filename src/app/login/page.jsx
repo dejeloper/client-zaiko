8c94856f8c8050f99efe36a1fd4e190dee843579
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2023-06-29 23:09:25 -0500

    Update Login Component

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,3 +1,5 @@
+import { Eye } from '@/components/icons'
+
 const Login = () => {
   return (
     <main className="flex justify-center items-center h-screen bg-gray-50">
@@ -12,7 +14,7 @@ const Login = () => {
           <form className="flex flex-col gap-4">
             <input
               type="text"
-              className="p-2 mt-8 rounded-xl border"
+              className="p-2 mt-8 rounded-lg border"
               id="user"
               name="user"
               placeholder="Usuario"
@@ -20,23 +22,33 @@ const Login = () => {
             <div className="relative">
               <input
                 type="password"
-                className="p-2 rounded-xl border w-full"
+                className="p-2 rounded-lg border w-full"
                 id="password"
                 name="password"
-                placeholder="Contrseña"
+                placeholder="Contraseña"
               />
+              <Eye size={22} color={'gray'} />
             </div>
             <button className="bg-principal rounded-xl text-white py-2">
               Ingresar
             </button>
           </form>
 
-          <div className="hidden">
-            <div className=" mt-6 grid grid-cols-3 items-center text-gray-400">
-              <hr className="border-gray-400" />
-              <p className="text-center text-sm">OR</p>
-              <hr className="border-gray-400" />
-            </div>
+          <div className=" mt-6 grid grid-cols-3 items-center text-gray-400">
+            <hr className="border-gray-400" />
+            <p className="text-center text-sm">OR</p>
+            <hr className="border-gray-400" />
+          </div>
+
+          <div className="mt-5 text-xs border-b border-[#002D74] py-4 text-[#002D74]">
+            <a href="#">¿Olvidó su contraseña?</a>
+          </div>
+
+          <div className="mt-3 text-xs flex justify-between items-center text-[#002D74]">
+            <p>¿No tiene cuenta?</p>
+            <button className="py-2 px-5 bg-white border rounded-xl hover:scale-110 duration-300">
+              Regístrese
+            </button>
           </div>
         </div>
 
